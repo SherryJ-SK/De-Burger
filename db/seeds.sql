84311fcd1aeb9a59fafe7737d5a2ded3df2420c6
Sherry <a-sherry@live.com>
2020-02-18 23:55:30 +1100

    preparation

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+SELECT * FROM burgers_db.burgers;
+INSERT INTO burgers (burger_name) VALUES ('Pickle max');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese master');
